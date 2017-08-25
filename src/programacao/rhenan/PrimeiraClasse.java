9e783436a384fa2f1c9a9179217334493bee3939
aluno <aluno@MSCGRESL04015.estacio.acad>
2017-08-24 21:16:33 -0300

    Exercio de declaracao de variaveis

---
+++ src/programacao/rhenan/PrimeiraClasse.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacao.rhenan;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class PrimeiraClasse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int variavelDoTipoInt = 10;
+        
+        System.out.println("Esta variavel é do tipo int: " + variavelDoTipoInt +" continuacao");
+        
+        //Como declarar variáveis
+        float posicaoDeMemoria;
+        
+        //Atribui um valor
+        posicaoDeMemoria = 10;
+        
+        
+        char caraCter, letra;
+        
+        
+        boolean verdadeiro=true, falso=false;
+        
+        if (variavelDoTipoInt == 10)
+            System.out.println("Declarar um numero int!!");
+        
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ src/programacao/rhenan/gh.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacao.rhenan;
+
+/**
+ *
+ * @author rhenan
+ */
+public class gh {
+
+    public static void main(String[] args) {
+        
+        // variavel do tipo inteiro
+        byte variavelDoTipoByte = 10;
+        short variavelDoTipoShort = 10;
+        int variavelDoTipoInt = 10;
+        
+        System.out.println("Está variavel do tipo int + variavelDoTipoByte()");
+        
+        if(variavelDoTipoByte == 10);
+        System.out.println("()");
+        
+        
+        
+        
+        
+        
+       
+        
+
+    }
+}
