1e6bd62b8e74409e47996a92c9a30efad1e263aa
aluno <aluno@MSCGRESL04015.estacio.acad>
2017-08-30 22:44:43 -0300

    Exercio de declaracao de variaveis

---
+++ src/programacao/rhenan/aula4/aula4.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacao.rhenan.aula4;
+
+/**
+ *
+ * @author aluno
+ */
+public class aula4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 2;
+        float y = 3.1f;
+
+        double z;
+
+        z = x * y;
+
+        System.out.println(z);
+        // TODO code application logic here
+    }
+
+}
