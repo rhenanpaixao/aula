8016a73a8820c59f69a9b99567bf1bcb4d6b475f
aluno <aluno@MSCGRESL04015.estacio.acad>
2017-08-30 23:05:31 -0300

    Multiplicacao de float/int

---
+++ src/programacao/rhenan/aula4/Aula4.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacao.rhenan.aula4;
+
+/**
+ *
+ * @author aluno
+ */
+public class Aula4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int idade = 23; // anos
+        int peso = 80; // kg
+        char sangue = 'B'; //tipo sanguineo
+        String Aniversario = "11/02/93"; //data do aniversario
+        int calcado = 41; // numero do sapato
+        boolean solteiro = true;
+
+        System.out.println("idade " + idade);
+        System.out.println("peso " + peso);
+        System.out.println("sangue " + sangue);
+        System.out.println("Aniversario " + Aniversario);
+        System.out.println("calcado " + calcado);
+        System.out.println("solteiro "+ solteiro);
+
+    }
+
+}
