a5d6582582c946b1cf68c5247728e9a02659bc1e
aluno <aluno@MSCGRESL04015.estacio.acad>
2017-08-31 21:14:55 -0300

    Teste Contenacao

---
+++ src/programacao/rhenan/aula4/TesteContenacao.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacao.rhenan.aula4;
+
+/**
+ *
+ * @author aluno
+ */
+public class TesteContenacao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String a = "String";
+        int b = 3;
+        int c = 7;
+
+        System.out.println(a + (b + c));
+
+        int d = 50;
+
+        System.out.println(a + (b + c) + d + a);
+
+    }
+}
