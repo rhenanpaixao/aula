af053795bdd511ffe958fb4544bcec0bed41b855
aluno <aluno@MSCGRESL04015.estacio.acad>
2017-09-06 22:49:21 -0300

    Sufixoprefixo1

--- src/Rhenan/Ex1.java
+++ src/Rhenan/Ex1.java
@@ -17,7 +17,7 @@ public class Ex1 {
     public static void main(String[] args) {
         int a = 10;
 
-        if (++a == 11) {
+        if (a++ == 11) {
             System.out.println("Sucesso" + a);
 
         } else {
